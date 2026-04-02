fba151f729a7a85368350941c76ecba50e4df0b7
niklasdahlen <dahlstromniklas1@gmail.com>
2026-04-02 14:54:14 +0200

    file/javascript

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let selectedProduct = null;
+let bsOrderModal;
+
+document.addEventListener("DOMContentLoaded", () => {
+  bsOrderModal = new bootstrap.Modal(document.getElementById("orderModal"));
+});
