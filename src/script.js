19977d342c5a9bdb60b1986b2d491b14f74120c6
niklasdahlen <dahlstromniklas1@gmail.com>
2026-04-03 11:53:05 +0200

    feature/javascriptmodal

--- src/script.js
+++ src/script.js
@@ -4,3 +4,17 @@ let bsOrderModal;
 document.addEventListener("DOMContentLoaded", () => {
   bsOrderModal = new bootstrap.Modal(document.getElementById("orderModal"));
 });
+
+// Close modal / reset
+document.getElementById("orderModal").addEventListener("hidden.bs.modal", () => {
+  clearForm();
+  document.getElementById("confirmBox").classList.add("d-none");
+  document.getElementById("orderForm").classList.remove("d-none");
+  document.getElementById("modalProductPreview").classList.add("d-none");
+  selectedProduct = null;
+});
+
+// Confirm button closes modal
+document.getElementById("confirmClose").addEventListener("click", () => {
+  bsOrderModal.hide();
+});
